9bc4b6485b8ceb4fa9d70d214bb8f4ce2e47bde8
Abitha <86874240+codecreation01@users.noreply.github.com>
2021-07-29 07:41:29 +0000

    Created  school  kit.py
    Gets input from the user and writes in csv file.

---
+++ School kit.py
@@ -0,0 +1,19 @@
+print ("basic school administration kit")
+import csv
+
+def write_into_csv (info_list):
+    with open('student_info.csv'.'w+'.'newline')as csv file:
+    writer=csv.writer(csv_file)
+    writer.writerow (info_list)
+condition= True
+while (condition):
+      student_info=input("enter student information in format(Name.age.contact_number.Email_I'd)")
+      print ("entered information:"+student_info)
+      student_info_list=student_info.split(" ")
+      print ("entered split up is:"+str(student_info_list))
+      write_into_csv (student_info_list)
+      condition_check=input("enter(yes/no) if you want to enter information:")
+      if condition_check=="yes":
+          condition=True
+      elif condition_check=="no"
+          condition=False
